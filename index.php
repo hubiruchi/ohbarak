b58f2e1c16789b75c9893e36c2a55e616d7ae725
Hillai <hillaib@walla.com>
2017-12-08 15:42:54 +0200

    ohbarak...

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+	<title>Oh, barak!</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="center">
+		<div class="button" id="ohbarak">oh barak</div>
+	</div>
+
+	<script>
+	var ohbaraks = <?php json_dumps(array_diff(scandir("audio"), array("..", "."))); ?>;
+	document.getElementById("ohbarak").addEventListener("click", function() {
+		var ohbarak = ohbaraks[Math.floor(Math.random()*ohbaraks.length)];
+		new Audio("audio/" + ohbarak + ".mp3").play();
+	});
+	</script>
+</body>
+</html>
