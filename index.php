ee4558fd313315f0f31bcaebf9fd3c93248dea5d
Hillai <hillaib@walla.com>
2017-12-08 16:05:59 +0200

    add barak's ohbarak

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ ini_set("display_errors", 1);
 	</div>
 
 	<script>
-	var ohbaraks = <?=json_encode(array_diff(scandir("audio"), array("..", ".")))?>;
+	var ohbaraks = <?=json_encode(array_values(array_diff(scandir("audio"), array("..", "."))))?>;
 	document.getElementById("ohbarak").addEventListener("click", function() {
 		var ohbarak = ohbaraks[Math.floor(Math.random()*ohbaraks.length)];
 		new Audio("audio/" + ohbarak).play();
