e5c572760374ce484488d39f09588aaf22416ef1
Hillai <hillaib@walla.com>
2017-12-09 17:51:10 +0200

    nsfw mode

--- index.php
+++ index.php
@@ -8,14 +8,24 @@
 </head>
 <body>
 	<div class="center">
-		<div class="button" id="ohbarak">oh barak</div>
+		<div class="button" id="ohbarak">oh barak</div><br><br>
+		<label><input type="checkbox" id="nsfw" checked> Include NSFW</label>
 	</div>
 
 	<script>
 	var ohbaraks = <?=json_encode(array_values(array_diff(scandir("audio"), array("..", "."))))?>;
+	var all_ohbaraks = ohbaraks;
+	
 	document.getElementById("ohbarak").addEventListener("click", function() {
 		var ohbarak = ohbaraks[Math.floor(Math.random()*ohbaraks.length)];
 		new Audio("audio/" + ohbarak).play();
+	});
+
+	document.getElementById("nsfw").addEventListener("change", function() {
+		if (this.checked)
+			ohbaraks = all_ohbaraks;
+		else
+			ohbaraks = ohbaraks.filter(name => !name.includes("nsfw"));
 	});
 	</script>
 </body>
